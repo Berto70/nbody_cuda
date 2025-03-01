5c85d05507391797f545f1922a2c8cc1ee89ff03
Berto70 <gabriele.bertinelli@studenti.unipd.it>
2025-03-01 15:45:07 +0000

    added and removed a lot of things. done some cleaning

--- parallel.cu
+++ parallel.cu
@@ -9,14 +9,16 @@
 #include <time.h>
 #include <chrono>
 
-#define N 16384  // number of particles 
-#define BLOCK_SIZE 256 // p <= N/40 
+#define N 16836  // number of particles 
+#define BLOCK_SIZE 512 // p <= N/40 
 #define G 1.0 // gravitational constant
 // #define DT 0.0000001 // time step 
 // #define EPSILON 0.00001 // softening parameter
 // #define STEPS 1000000 // simulation steps
 
-#define DT 0.0001f // time step 
+#define evolt 0.315f
+
+#define DT 0.000001f // time step 
 #define EPS2 1e-9f // softening parameter
 #define STEPS 1000 // simulation steps
 #define L 100.0 // box size
@@ -110,7 +112,7 @@ __device__ float3 tileCalculation(float4 myPos, float3 accel) {
 
     // Shared memory for positions of particles in the tile
     extern __shared__ float4 shPosition[];
-    
+    #pragma unroll 128
     for (int i = 0; i < blockDim.x; i++) {
         accel = computeAcceleration(myPos, shPosition[i], accel);
     }
@@ -169,60 +171,96 @@ __global__ void updateVelocities(int n, float4 *vel, float4 *oldAcc, float4 *new
     } 
 }
 
-int main() { 
+__global__ void computeEnergy(int n, float4 *pos, float4 *vel, float1 *totEn) {
+    float1 kinetic = {0.0f};
+    float1 potential = {0.0f};
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < n) {
+        #pragma unroll
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                float3 r;
+                r.x = pos[j].x - pos[i].x;
+                r.y = pos[j].y - pos[i].y;
+                r.z = pos[j].z - pos[i].z;
+                
+                float distSqr = r.x * r.x + r.y * r.y + r.z * r.z + EPS2;
+                float distSixth = distSqr * distSqr * distSqr;
+                potential.x -= (pos[i].w * pos[j].w) / sqrt(distSixth);
+            }
+        }
+    }
+
+    kinetic.x += 0.5 * pos[i].w * (vel[i].x * vel[i].x + vel[i].y * vel[i].y + vel[i].z * vel[i].z);
+    totEn->x = kinetic.x + potential.x;
+}
+
+void computePerfStats(double &interactionsPerSecond, double &gflops, float milliseconds, int iterations)
+{
+    const int flopsPerInteraction = 20;
+    interactionsPerSecond = (float)N * (float)N;
+    interactionsPerSecond *= 1e-9 * iterations * 1000 / milliseconds;
+    gflops = interactionsPerSecond * (float)flopsPerInteraction;
+
+}
+
+int returnPosition(int sims) { 
     // Host memory
     clock_t memGen_in = clock();
     float4 *h_pos = (float4*)malloc(N * sizeof(float4));
     float4 *h_vel = (float4*)malloc(N * sizeof(float4));
     float3 *com = (float3*)malloc(sizeof(float3));
+    float1 *h_totEn = (float1*)malloc(sizeof(float1));
 
     // Initialize data
-    double mass_range[2] = {1.0, 10.0};      // Masses between 1.0 and 10.0
-    double pos_range[2] = {-50.0, 50.0};     // Positions between -50.0 and 50.0 --> L = 100.0
-    double vel_range[2] = {-1.0, 1.0};       // Velocities between -1.0 and 1.0
+    if (sims > 3) {
 
-    ic_random_uniform(N, mass_range, pos_range, vel_range, h_pos, h_vel, 1);
-    clock_t memGen_out = clock();
-    double memGen_time = (double)(memGen_out - memGen_in) / CLOCKS_PER_SEC;
-    std::cout << "Host mem alloc + body gen: " << memGen_time << " s\n";
+        double mass_range[2] = {1.0, 10.0};      // Masses between 1.0 and 10.0
+        double pos_range[2] = {-50.0, 50.0};     // Positions between -50.0 and 50.0 --> L = 100.0
+        double vel_range[2] = {-1.0, 1.0};       // Velocities between -1.0 and 1.0
 
-    // two-body sys
-    /* h_pos[0].x = 0.0; h_pos[0].y = 0.0; h_pos[0].z = 0.0; h_pos[0].w = 8.0;
-    h_pos[1].x = 0.1; h_pos[1].y = 0.0; h_pos[1].z = 0.0; h_pos[1].w = 2.0;
-    h_vel[0].x = 0.0; h_vel[0].y = -2.0; h_vel[0].z = 0.0; h_vel[0].w = 0.0;
-    h_vel[1].x = 0.0; h_vel[1].y = 8.0; h_vel[1].z = 0.0; h_vel[1].w = 0.0; */
+        ic_random_uniform(N, mass_range, pos_range, vel_range, h_pos, h_vel, 1);
+        clock_t memGen_out = clock();
+        double memGen_time = (double)(memGen_out - memGen_in) / CLOCKS_PER_SEC;
+        std::cout << "Host mem alloc + body gen: " << memGen_time << " s\n";
+    
+    } else if (sims == 2) {
 
-    // three-body sys
+        // two-body sys
+        h_pos[0].x = 0.0; h_pos[0].y = 0.0; h_pos[0].z = 0.0; h_pos[0].w = 8.0;
+        h_pos[1].x = 0.1; h_pos[1].y = 0.0; h_pos[1].z = 0.0; h_pos[1].w = 2.0;
+        h_vel[0].x = 0.0; h_vel[0].y = -2.0; h_vel[0].z = 0.0; h_vel[0].w = 0.0;
+        h_vel[1].x = 0.0; h_vel[1].y = 8.0; h_vel[1].z = 0.0; h_vel[1].w = 0.0;
 
-    /* h_pos[0].x = 1.0; h_pos[0].y = 3.0; h_pos[0].z = 0.0; h_pos[0].w = 3.0;
-    h_pos[1].x = -2.0; h_pos[1].y = -1.0; h_pos[1].z = 0.0; h_pos[1].w = 4.0;
-    h_pos[2].x = 1.0; h_pos[2].y = -1.0; h_pos[2].z = 0.0; h_pos[2].w = 5.0;
-    h_vel[0].x = 0.0; h_vel[0].y = 0.0; h_vel[0].z = 0.0; h_vel[0].w = 0.0;
-    h_vel[1].x = 0.0; h_vel[1].y = 0.0; h_vel[1].z = 0.0; h_vel[1].w = 0.0;
-    h_vel[2].x = 0.0; h_vel[2].y = 0.0; h_vel[2].z = 0.0; h_vel[2].w = 0.0; */
+    } else if (sims == 3) {
 
-    /* h_mass[0]=3;h_mass[1]=4;h_mass[2]=5;
-    h_posX[0]=1;h_posX[1]=-2;h_posX[2]=1;
-    h_posY[0]=3;h_posY[1]=-1;h_posY[2]=-1;
-    h_posZ[0]=h_posZ[1]=h_posZ[2]=0;
-    h_velX[0]=h_velX[1]=h_velX[2]=0;
-    h_velY[0]=h_velY[1]=h_velY[2]=0;
-    h_velZ[0]=h_velX[1]=h_velX[2]=0; */
+        // three-body sys
+        h_pos[0].x = 1.0; h_pos[0].y = 3.0; h_pos[0].z = 0.0; h_pos[0].w = 3.0;
+        h_pos[1].x = -2.0; h_pos[1].y = -1.0; h_pos[1].z = 0.0; h_pos[1].w = 4.0;
+        h_pos[2].x = 1.0; h_pos[2].y = -1.0; h_pos[2].z = 0.0; h_pos[2].w = 5.0;
+        h_vel[0].x = 0.0; h_vel[0].y = 0.0; h_vel[0].z = 0.0; h_vel[0].w = 0.0;
+        h_vel[1].x = 0.0; h_vel[1].y = 0.0; h_vel[1].z = 0.0; h_vel[1].w = 0.0;
+        h_vel[2].x = 0.0; h_vel[2].y = 0.0; h_vel[2].z = 0.0; h_vel[2].w = 0.0;
+    }
 
     clock_t memAlloc_in = clock();
     // Device memory - using float4 for positions, velocities and accelerations
     float4 *d_pos, *d_vel, *d_acc_old, *d_acc_new;
+    float1 *d_totEn;
     
     // Allocate memory for float4 arrays
     cudaMalloc(&d_pos, N * sizeof(float4));
     cudaMalloc(&d_vel, N * sizeof(float4));
     cudaMalloc(&d_acc_old, N * sizeof(float4));
     cudaMalloc(&d_acc_new, N * sizeof(float4));
+    cudaMalloc(&d_totEn, sizeof(float1));
     
     
     // Copy initial data to device
     cudaMemcpy(d_pos, h_pos, N * sizeof(float4), cudaMemcpyHostToDevice);
     cudaMemcpy(d_vel, h_vel, N * sizeof(float4), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_totEn, h_totEn, sizeof(float1), cudaMemcpyHostToDevice);
     
     // Initialize old acceleration to zero
     cudaMemset(d_acc_old, 0, N * sizeof(float4));
@@ -230,7 +268,7 @@ int main() {
     double memAlloc_time = (double)(memAlloc_out - memAlloc_in) / CLOCKS_PER_SEC;
     std::cout << "Device mem alloc: " << memAlloc_time << " s\n";
 
-    /* FILE *fp = fopen("boh_output.csv", "w");
+    /* FILE *fp = fopen("parallel_output.csv", "w");
     if (!fp) {
         printf("Error opening file\n");
         return 1;
@@ -239,7 +277,125 @@ int main() {
     fseek(fp, 0, SEEK_END);
     if (ftell(fp) == 0) {
         fprintf(fp, "ID,t_step,pos_x,pos_y,pos_z\n");
+    }
+
+    FILE *fpe = fopen("parallel_energy.csv", "w");
+    if (!fpe) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    // If the file is empty, write the header.
+    fseek(fpe, 0, SEEK_END);
+    if (ftell(fpe) == 0) {
+        fprintf(fpe, "t_step,e_tot\n");
     } */
+
+    int numBlocks = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    int sharedMemSize = BLOCK_SIZE * sizeof(float4);
+
+    for (int step = 0; step < STEPS; step++) {
+        // Calculate forces and update accelerations
+        forceCalculation<<<numBlocks, BLOCK_SIZE, sharedMemSize>>>(d_pos, d_acc_new);
+        
+        // Update positions
+        updatePositions<<<numBlocks, BLOCK_SIZE>>>(N, d_pos, d_vel, d_acc_old, DT);
+        
+        // Update velocities
+        updateVelocities<<<numBlocks, BLOCK_SIZE>>>(N, d_vel, d_acc_old, d_acc_new, DT);
+
+        /* computeEnergy<<<numBlocks, BLOCK_SIZE>>>(N, d_pos, d_vel, d_totEn);
+        cudaMemcpy(h_totEn, d_totEn, sizeof(float1), cudaMemcpyDeviceToHost); */
+        
+        // Copy new acceleration to old acceleration
+        cudaMemcpy(d_acc_old, d_acc_new, N * sizeof(float4), cudaMemcpyDeviceToDevice);
+
+        cudaMemcpy(h_pos, d_pos, N * sizeof(float4), cudaMemcpyDeviceToHost);
+        /* if (step % 100 == 0) {
+            fprintf(fpe,"%d,%.6f\n", step, h_totEn->x);
+            for (int i = 0; i < N; i++) {
+                fprintf(fp, "%d,%d,%.6f,%.6f,%.6f\n",
+                    i, step, h_pos[i].x, h_pos[i].y, h_pos[i].z);
+            }
+        } */
+    }
+
+    /* fclose(fp);
+    fclose(fpe); */
+
+    // Cleanup
+    cudaFree(d_pos);
+    cudaFree(d_vel);
+    cudaFree(d_acc_old);
+    cudaFree(d_acc_new);
+    cudaFree(d_totEn);
+    
+    free(h_pos);
+    free(h_vel);
+    free(com);
+    free(h_totEn);
+
+    return 0;
+}
+
+int returnEnergy() {
+    // Host memory
+    clock_t memGen_in = clock();
+    float4 *h_pos = (float4*)malloc(N * sizeof(float4));
+    float4 *h_vel = (float4*)malloc(N * sizeof(float4));
+    float3 *com = (float3*)malloc(sizeof(float3));
+    float1 *h_totEn = (float1*)malloc(sizeof(float1));
+
+    /* // Initialize data
+    double mass_range[2] = {1.0, 10.0};      // Masses between 1.0 and 10.0
+    double pos_range[2] = {-50.0, 50.0};     // Positions between -50.0 and 50.0 --> L = 100.0
+    double vel_range[2] = {-1.0, 1.0};       // Velocities between -1.0 and 1.0
+
+    ic_random_uniform(N, mass_range, pos_range, vel_range, h_pos, h_vel, 1); */
+
+    // two-body sys
+    h_pos[0].x = 0.0; h_pos[0].y = 0.0; h_pos[0].z = 0.0; h_pos[0].w = 8.0;
+    h_pos[1].x = 0.1; h_pos[1].y = 0.0; h_pos[1].z = 0.0; h_pos[1].w = 2.0;
+    h_vel[0].x = 0.0; h_vel[0].y = -2.0; h_vel[0].z = 0.0; h_vel[0].w = 0.0;
+    h_vel[1].x = 0.0; h_vel[1].y = 8.0; h_vel[1].z = 0.0; h_vel[1].w = 0.0;
+
+    clock_t memGen_out = clock();
+    double memGen_time = (double)(memGen_out - memGen_in) / CLOCKS_PER_SEC;
+    std::cout << "Host mem alloc + body gen: " << memGen_time << " s\n";
+
+    clock_t memAlloc_in = clock();
+    // Device memory - using float4 for positions, velocities and accelerations
+    float4 *d_pos, *d_vel, *d_acc_old, *d_acc_new;
+    float1 *d_totEn;
+    
+    // Allocate memory for float4 arrays
+    cudaMalloc(&d_pos, N * sizeof(float4));
+    cudaMalloc(&d_vel, N * sizeof(float4));
+    cudaMalloc(&d_acc_old, N * sizeof(float4));
+    cudaMalloc(&d_acc_new, N * sizeof(float4));
+    cudaMalloc(&d_totEn, sizeof(float1));
+    
+    
+    // Copy initial data to device
+    cudaMemcpy(d_pos, h_pos, N * sizeof(float4), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_vel, h_vel, N * sizeof(float4), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_totEn, h_totEn, sizeof(float1), cudaMemcpyHostToDevice);
+    
+    // Initialize old acceleration to zero
+    cudaMemset(d_acc_old, 0, N * sizeof(float4));
+    clock_t memAlloc_out = clock();
+    double memAlloc_time = (double)(memAlloc_out - memAlloc_in) / CLOCKS_PER_SEC;
+    std::cout << "Device mem alloc: " << memAlloc_time << " s\n";
+
+    FILE *fp = fopen("parallel_energy.csv", "w");
+    if (!fp) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    // If the file is empty, write the header.
+    fseek(fp, 0, SEEK_END);
+    if (ftell(fp) == 0) {
+        fprintf(fp, "t_step,e_tot\n");
+    }
 
 
     auto beginTime = std::chrono::high_resolution_clock::now();
@@ -257,23 +413,19 @@ int main() {
         
         // Update velocities
         updateVelocities<<<numBlocks, BLOCK_SIZE>>>(N, d_vel, d_acc_old, d_acc_new, DT);
+
+        // Compute total energy
+        computeEnergy<<<numBlocks, BLOCK_SIZE>>>(N, d_pos, d_vel, d_totEn);
+
+        cudaMemcpy(h_totEn, d_totEn, sizeof(float1), cudaMemcpyDeviceToHost);
         
         // Copy new acceleration to old acceleration
         cudaMemcpy(d_acc_old, d_acc_new, N * sizeof(float4), cudaMemcpyDeviceToDevice);
         
-        /* // Optionally, if output is needed at each step:
-        if (step % 1000 == 0) {  // Output every 1000 steps
-            cudaMemcpy(h_pos, d_pos, N * sizeof(float4), cudaMemcpyDeviceToHost);
-            // Update center of mass calculation or output as needed
-        } */
-
-        cudaMemcpy(h_pos, d_pos, N * sizeof(float4), cudaMemcpyDeviceToHost);
-        /* if (step % 100 == 0) {
-            for (int i = 0; i < N; i++) {
-                fprintf(fp, "%d,%d,%.6f,%.6f,%.6f\n",
-                    i, step, h_pos[i].x, h_pos[i].y, h_pos[i].z);
-            }
-        } */
+        if (step % 100 == 0) {
+            fprintf(fp,"%d,%.6f\n", step, h_totEn->x);\
+        }
+        
     }
     clock_t kernel_out = clock();
     double kernel_time = (double)(kernel_out - kernel_in) / CLOCKS_PER_SEC;
@@ -287,31 +439,47 @@ int main() {
     std::cout << "Time: " << ms << " ms\n";
     std::cout << "GFLOPS: " << gflops << std::endl;
 
-    /* fclose(fp); */
-    
-    // // Copy final positions back for analysis
-    // cudaMemcpy(h_pos, d_pos, N * sizeof(float4), cudaMemcpyDeviceToHost);
-    // cudaMemcpy(h_vel, d_vel, N * sizeof(float4), cudaMemcpyDeviceToHost);
-    
-    // // Copy float4 data back to separate arrays if needed
-    // for (int i = 0; i < N; i++) {
-    //     h_posX[i] = h_pos[i].x;
-    //     h_posY[i] = h_pos[i].y;
-    //     h_posZ[i] = h_pos[i].z;
-    //     h_velX[i] = h_vel[i].x;
-    //     h_velY[i] = h_vel[i].y;
-    //     h_velZ[i] = h_vel[i].z;
-    // }
+    fclose(fp);
 
     // Cleanup
     cudaFree(d_pos);
     cudaFree(d_vel);
     cudaFree(d_acc_old);
     cudaFree(d_acc_new);
+    cudaFree(d_totEn);
     
     free(h_pos);
     free(h_vel);
     free(com);
+    free(h_totEn);
 
     return 0;
 }
+
+void runBenchmark(int iterations)
+{
+    // once without timing to prime the GPU
+    returnPosition(N);
+
+    clock_t t_start = clock();  
+        returnPosition(N);
+    clock_t t_end = clock();  
+
+    float milliseconds = ((float)t_end - (float)t_start) / CLOCKS_PER_SEC * 1000;
+    double interactionsPerSecond = 0;
+    double gflops = 0;
+    computePerfStats(interactionsPerSecond, gflops, milliseconds, iterations);
+    
+    printf("%d bodies, total time for %d iterations: %0.3f ms\n", 
+           N, iterations, milliseconds);
+    printf("= %0.3f billion interactions per second\n", interactionsPerSecond);
+    printf("= %0.3f GFLOP/s at %d flops per interaction\n", gflops, 20);
+    
+}
+
+int main() {
+    // returnPosition();
+    // returnEnergy();
+    runBenchmark(STEPS);
+    return 0;
+} 
